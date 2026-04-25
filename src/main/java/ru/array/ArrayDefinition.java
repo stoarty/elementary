b95b19dec6e297d01bcca184da170d64af310d26
susanna.tokhchukova@telekom.com <Susanna.Tokhchukova@telekom.com>
2026-04-25 21:05:15 +0300

    5.3 Заполнение массива

--- src/main/java/ru/array/ArrayDefinition.java
+++ src/main/java/ru/array/ArrayDefinition.java
@@ -6,8 +6,17 @@ public class ArrayDefinition {
         short[] ages = new short[5];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "Ivan Ivanov";
+        names[1] = "Petr Petrov";
+        names[2] = "Anna Annina";
+        names[3] = "Marina Marinina";
         System.out.println("ages length = " + ages.length);
         System.out.println("surnames length = " + surnames.length);
         System.out.println("prices length = " + prices.length);
+        for (String name : names) {
+            System.out.println(name);
+        }
     }
+
 }
