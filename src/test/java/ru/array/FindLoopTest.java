5763a90fd5f7331f8cfc588646bc9c862d440d3d
susanna.tokhchukova@telekom.com <Susanna.Tokhchukova@telekom.com>
2026-04-25 22:01:56 +0300

    5.8 Классический поиск перебором

---
+++ src/test/java/ru/array/FindLoopTest.java
@@ -0,0 +1,26 @@
+package ru.array;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class FindLoopTest {
+    @Test
+    void whenArrayHas5Then0() {
+        int[] data = new int[] {5, 10, 3};
+        int el = 5;
+        int result = FindLoop.indexOf(data, el);
+        int expected = 0;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenArrayHasNot10ThenMinus1() {
+        /* Здесь нужно дописать тест, когда в массиве не найдено число 10.
+        Можно искать любое другое число, корректно изменив при этом название теста */
+        int[] data = new int[] {5, 11, 3};
+        int el = 10;
+        int result = FindLoop.indexOf(data, el);
+        int expected = -1;
+        assertThat(result).isEqualTo(expected);
+    }
+}
