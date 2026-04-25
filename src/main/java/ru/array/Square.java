fcaebedfab5653d4783f013464054c2fd411f726
susanna.tokhchukova@telekom.com <Susanna.Tokhchukova@telekom.com>
2026-04-25 21:33:08 +0300

    5.6 Заполнить_массив_степенями_чисел

---
+++ src/main/java/ru/array/Square.java
@@ -0,0 +1,19 @@
+package ru.array;
+
+public class Square {
+    public static int[] calculate(int bound) {
+        int[] rsl = new int[bound];
+        for (int index = 0; index < rsl.length; index++) {
+            rsl[index] = index * index;
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        int[] array = calculate(4);
+        for (int index = 0; index < array.length; index++) {
+            System.out.println(array[index]);
+        }
+    }
+}
+
