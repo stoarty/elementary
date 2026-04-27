d0caeaedfb6ba80a92939154c3a9d6a52c62fa8b
susanna.tokhchukova@telekom.com <Susanna.Tokhchukova@telekom.com>
2026-04-27 13:29:02 +0300

    5.12 Перевернуть массив

---
+++ src/main/java/ru/array/Turn.java
@@ -0,0 +1,14 @@
+package ru.array;
+
+public class Turn {
+    public static int[] back(int[] array) {
+        int n = array.length;
+        for (int i = 0; i < n / 2; i++) {
+            int temp = array[i];
+            array[i] = array[n - 1 - i];
+            array[n - 1 - i] = temp;
+        }
+        return array;
+    }
+}
+
