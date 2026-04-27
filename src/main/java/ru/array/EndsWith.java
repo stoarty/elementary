a9d3ed0e5b01e58b36fdcb5c7ff8ef9533eaa8e2
susanna.tokhchukova@telekom.com <Susanna.Tokhchukova@telekom.com>
2026-04-27 15:14:47 +0300

    fix: 5.17 Слово заканчивается на ...

--- src/main/java/ru/array/EndsWith.java
+++ src/main/java/ru/array/EndsWith.java
@@ -3,7 +3,7 @@ package ru.array;
 public class EndsWith {
     public static boolean endsWith(char[] word, char[] post) {
         for (int i = 0; i < post.length; i++) {
-            if (word[word.length - post.length + i] != post[i]) {
+            if (word[word.length - 1 - i] != post[post.length - 1 - i]) {
                 return false;
             }
         }
