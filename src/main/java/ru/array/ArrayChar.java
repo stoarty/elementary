f75a10b5b3a5a8b98315cd8e30c8fa3c0aa56c69
susanna.tokhchukova@telekom.com <Susanna.Tokhchukova@telekom.com>
2026-04-27 13:56:50 +0300

    5.14 Слово начинается с ...

---
+++ src/main/java/ru/array/ArrayChar.java
@@ -0,0 +1,14 @@
+package ru.array;
+
+public class ArrayChar {
+
+    public static boolean startsWith(char[] word, char[] pref) {
+        for (int i = 0; i < pref.length; i++) {
+            if (word[i] != pref[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
