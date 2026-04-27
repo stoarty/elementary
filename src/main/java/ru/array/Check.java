247519e0ae0d4500b6a2e345d9d995bfdbc3c8f7
susanna.tokhchukova@telekom.com <Susanna.Tokhchukova@telekom.com>
2026-04-27 13:46:36 +0300

    5.13 Массив заполнен true или false

---
+++ src/main/java/ru/array/Check.java
@@ -0,0 +1,12 @@
+package ru.array;
+
+public class Check {
+    public static boolean mono(boolean[] data) {
+        for (int i = 1; i < data.length; i++) {
+            if (data[0] != data[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
