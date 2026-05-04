b60ca49db5cff0b8924f4862f2dfc2465728f449
susanna.tokhchukova@telekom.com <susanna.tokhchukova@telekom.com>
2026-05-04 21:20:05 +0300

    fix 5.18: remove unnesessary variable

--- src/main/java/ru/array/FindLoop.java
+++ src/main/java/ru/array/FindLoop.java
@@ -2,23 +2,21 @@ package ru.array;
 
 public class FindLoop {
     public static int indexOf(int[] data, int el) {
-        int rst = -1;
         for (int index = 0; index < data.length; index++) {
             if (data[index] == el) {
                 return index;
             }
         }
-        return rst;
+        return -1;
     }
 
     public static int indexInRange(int[] data, int el, int start, int finish) {
-        int rst = -1;
         for (int index = start; index <= finish; index++) {
             if (data[index] == el) {
                 return index;
             }
         }
-        return rst;
+        return -1;
     }
 }
 
