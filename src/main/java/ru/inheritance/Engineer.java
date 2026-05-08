c4d78d147f87e0b826922ed63a2b1eebfa6f3de7
susanna.tokhchukova@telekom.com <Susanna.Tokhchukova@telekom.com>
2026-05-08 14:01:05 +0300

    8.2 Вызов_конструктора_родителя_super_.docx

---
+++ src/main/java/ru/inheritance/Engineer.java
@@ -0,0 +1,11 @@
+package ru.inheritance;
+
+public class Engineer extends Profession {
+    private int experience;
+
+    public Engineer(int experience, boolean degree) {
+        super(degree);
+        this.experience = experience;
+    }
+}
+
---
+++ src/main/java/ru/inheritance/Profession.java
@@ -0,0 +1,10 @@
+package ru.inheritance;
+
+public class Profession {
+    private boolean degree;
+
+    public Profession(boolean degree) {
+        this.degree = degree;
+    }
+}
+
